4f9f2e1ac401de20ac329ecca1ef7cf2b0d21a3f
grro <>
2023-07-14 18:43:20 +0200

    http session handling improved

--- ahoy_dtu_webthing/dtu.py
+++ ahoy_dtu_webthing/dtu.py
@@ -42,26 +42,36 @@ class Inverter:
         self.is_available = False
         self.is_producing = False
         self.listener = None
+        self.session = Session()
         Thread(target=self.__periodic_refresh, daemon=True).start()
 
     def close(self):
         self.is_running = False
 
+    def __renew_session(self):
+        try:
+            self.session.close()
+        except Exception as e:
+            logging.warning("error occurred closing session " + str(e))
+        self.session = Session()
+
     def __periodic_refresh(self):
-        session = Session()
         while self.is_running:
             try:
                 sleep(randint(0, self.interval))
-                self.refresh(session)
+                self.refresh()
                 sleep(int(self.interval/2))
             except Exception as e:
                 logging.warning("error occurred refreshing inverter " + self.name + " " + str(e) + " (max " + str(self.power_max) + " watt)")
                 sleep(5)
-                session = Session()
+                try:
+                    self.__renew_session()
+                except Exception as e:
+                    logging.warning("error occurred renewing session " + str(e))
 
-    def refresh(self, session: Session):
+    def refresh(self):
         # fetch inverter info
-        response = session.get(self.index_uri, timeout=60)
+        response = self.session.get(self.index_uri, timeout=60)
         inverter_state = response.json()['inverter']
 
         previous_is_available = self.is_available
@@ -76,15 +86,15 @@ class Inverter:
 
         if self.is_producing:
             # fetch power limit
-            response = session.get(self.config_uri, timeout=60)
+            response = self.session.get(self.config_uri, timeout=60)
             inverter_configs = response.json()['inverter']
 
             # fetch inverter info
-            response = session.get(self.inverter_uri, timeout=60)
+            response = self.session.get(self.inverter_uri, timeout=60)
             inverter_infos = response.json()['inverter']
 
             # fetch temp, power, etc
-            response = session.get(self.live_uri, timeout=60)
+            response = self.session.get(self.live_uri, timeout=60)
             inverter_measures = response.json()['inverter']
 
             p_ac = 0
@@ -142,6 +152,7 @@ class Inverter:
             self.update(power_max, power_limit, p_ac, u_ac, i_ac, p_dc, p_dc1, p_dc2, u_dc1, u_dc2, i_dc1, i_dc2, efficiency, temp, frequency)
         else:
             self.update(self.power_max, self.power_limit, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
     def set_power_limit(self, limit_watt: int):
         logging.info("inverter " + self.name + " set power limit to " + str(limit_watt) + " watt")
         requests.post(self.update_uri, json={"id": self.id, "cmd": "limit_nonpersistent_absolute", "val": limit_watt})
